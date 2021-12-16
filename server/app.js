26638d570d6969bfe182629d7978aa0534d58c3e
mmonereo <mauro.monereo@gmail.com>
2021-12-16 11:33:25 +0100

    deploy 2

--- server/app.js
+++ server/app.js
@@ -1,28 +1,14 @@
-const path = require('path')
-require("dotenv/config");
-
-
-require("./db");
-
-
 const express = require("express");
+const app = express();const path = require('path')
 
-const app = express();
-
-
+require("dotenv/config");
+require("./db");
 require("./config")(app);
 
 
 app.use(express.static(path.join(__dirname, "public")))
-
-
 require("./routes")(app);
-
-
 app.use((req, res) => res.sendFile(__dirname + "/public/index.html"));
 
-
-
-require("./error-handling")(app);
 
 module.exports = app;
